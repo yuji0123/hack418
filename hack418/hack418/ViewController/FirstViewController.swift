7cd9eab4a95a2baf50c718649af2708b809a8733
YusukeMorishita <yusuke.0213@gmail.com>
2015-10-03 17:15:47 +0900

    フレンド検索Viewの追加

--- hack418/hack418/ViewController/FirstViewController.swift
+++ hack418/hack418/ViewController/FirstViewController.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import CoreLocation
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, CLLocationManagerDelegate {
+    
+    
+    // 位置情報取得
+    var lm: CLLocationManager!
+    var latitude: CLLocationDegrees! // 取得した緯度を保持するインスタンス
+    var longitude: CLLocationDegrees! // 取得した経度を保持するインスタンス
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // フィールドの初期化
+        lm = CLLocationManager()
+        longitude = CLLocationDegrees()
+        latitude = CLLocationDegrees()
+        
+        // CLLocationManagerをDelegateに指定
+        lm.delegate = self
+        
+        // 位置情報取得の許可を求めるメッセージの表示．必須．
+        lm.requestAlwaysAuthorization()
+        // 位置情報の精度を指定．任意，
+        // lm.desiredAccuracy = kCLLocationAccuracyBest
+        // 位置情報取得間隔を指定．指定した値（メートル）移動したら位置情報を更新する．任意．
+        // lm.distanceFilter = 1000
+        
+        // GPSの使用を開始する
+        lm.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +46,23 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /* 位置情報取得成功時に実行 */
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation){
+        // 緯度の取得
+        latitude = newLocation.coordinate.latitude
+        // 経度の取得
+        longitude = newLocation.coordinate.longitude
 
+        NSLog("緯度: \(latitude) , 経度: \(longitude)")
+        
+        // GPSの使用を停止する．停止しない限りGPSは実行され，指定間隔で更新され続ける．
+        // lm.stopUpdatingLocation()
+    }
+    
+    /* 位置情報取得失敗時に実行 */
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        // この例ではLogにErrorと表示するだけ．
+        NSLog("Error")
+    }
 }
 
---
+++ hack418/hack418/ViewController/FriendSearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendSearchTableViewCell.swift
+//  hack418
+//
+//  Created by anthony on 2015/10/03.
+//  Copyright © 2015年 teamB. All rights reserved.
+//
+
+import UIKit
+
+class FriendSearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
